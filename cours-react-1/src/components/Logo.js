67c23250dd0e50f0a93b044bb34ed9efe89871a0
Lou-Anne Lecocq <115995772+louanne622@users.noreply.github.com>
2023-04-07 12:57:32 +0200

    css navbar + header logo + text

---
+++ cours-react-1/src/components/Logo.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Logo = () => {
+    return (
+        <div className='logo'>
+            <img src="./logo.png" alt="" width="100px" height="100px" />
+            <h1>Fly</h1>
+        </div>
+    );
+};
+
+export default Logo;
--- cours-react-1/src/pages/Home.js
+++ cours-react-1/src/pages/Home.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import Navigation from '../components/Navigation';
+import Logo from '../components/Logo';
 
 const Home = () => {
     return (
         <div>
+            <Logo/>
             <Navigation/>
             <h1>Accueil</h1>
         </div>
