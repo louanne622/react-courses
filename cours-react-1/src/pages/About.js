26f62aa6c141e9784d648f74e2e0750371da7d61
Lou-Anne Lecocq <115995772+louanne622@users.noreply.github.com>
2023-04-07 12:07:03 +0200

    initialisation + ajout navbar

---
+++ cours-react-1/src/pages/About.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import Navigation from '../components/Navigation';
+
+const About = () => {
+    return (
+        <div>
+            <Navigation />
+            <h1>A propos</h1>
+            <br />
+            <p>
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Cum itaque voluptatibus quo rem eveniet, temporibus inventore, in blanditiis tenetur maiores aliquam? Voluptate animi, deserunt temporibus cumque aliquid provident natus doloribus.
+            </p>
+            <br />
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores placeat eum culpa labore, aut magnam atque repellat! Architecto, optio reprehenderit blanditiis accusamus consectetur, velit amet vel eius tenetur vero debitis?
+            </p>
+        </div>
+    );
+};
+
+export default About;
