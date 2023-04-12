c33265d7985b3ed442c6750048aa9a51dbbf6102
Lou-Anne Lecocq <115995772+louanne622@users.noreply.github.com>
2023-04-12 10:38:27 +0200

    add error message

--- cours-react-1/src/components/Countries.js
+++ cours-react-1/src/components/Countries.js
@@ -8,19 +8,24 @@ const Countries = () => {
 
 // pagination
 const [currentPage, setCurrentPage] = useState(1);
-const [flagsPerPage, setflagsPerPage] = useState(16);
+const [flagsPerPage, setflagsPerPage] = useState(15);
 
     // Pour modifier data, il faudra passer par setDat
 const [data, setData] = useState([]);
 const [loading, setLoading] = useState(false);
-const [error, setErrors] = useState([]);
+const [errorMessage, setErrorMessage] = useState([]);
 // le useEffet se jour lorsque le composant est monté
 useEffect(() => {
-    axios.get('https://restcountries.com/v3.1/all')
-    .then((res) =>
-        setData(res.data));
+    const responce = axios.get('https://restcountries.com/v3.1/all')
+    .then(res => {
+        setData(res.data);
         setLoading(true);
-
+        })
+    .catch(error => {
+    setErrorMessage('Désolée, il y a eu une erreur lors de la récupération des données');
+    setLoading(true);
+    console.log(error)
+    });
 }, []);
 
 // Paganisation
@@ -32,6 +37,9 @@ const currentFlags = data.slice(firstFlagIndex, lastFlagIndex);
     return (
         <div className='countries'>
             <h1>Countries</h1>
+            {errorMessage && (
+            <p className="error"> {errorMessage} </p>
+            )}
             <ul>
             {loading ? currentFlags.map((country, index) =>{
                 return <Card key={index} country={country}/>
